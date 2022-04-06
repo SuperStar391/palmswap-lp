f7c81fedfe25ced9a002c206fba6cbe0e2b7bc0e
collin <collin@palmswap.org>
2022-04-07 02:42:08 +0800

    fix issue

--- src/components/RoadmapItem.tsx
+++ src/components/RoadmapItem.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { Typography, useMediaQuery } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Box, useTheme } from "@mui/system";
 import { IRoadmapItem } from "interfaces/IRoadmapItem";
 
